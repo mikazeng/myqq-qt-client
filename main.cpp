3b9d5e8e40ff03b452efff776795394592818740
32875094@qq.com <32875094@qq.com>
2018-02-12 16:53:05 +0800

    这是main函数

---
+++ main.cpp
@@ -0,0 +1,24 @@
+﻿#include <QApplication>
+#include <QMessageBox>
+#include "toolbox.h"
+#include "json/json.h"
+#include <iostream>
+#include <QString>
+#include <qDebug>
+using namespace std;
+using namespace Json;
+extern "C"{
+#include "openssl/applink.c"
+};
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+     toolbox*q= new toolbox;
+   // Json::Value val;
+
+    //   val["name"] = 123456;
+    //   val["pw"] = "我是张三";
+    //qDebug()<<val.toStyledString().c_str();
+
+    return a.exec();
+}
