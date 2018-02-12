561e6c2d9664038cde43a53af0f9c4dcdc2cc4d2
32875094@qq.com <32875094@qq.com>
2018-02-12 16:51:03 +0800

    这是登录界面的源代码

---
+++ login.cpp
@@ -0,0 +1,103 @@
+﻿#include "login.h"
+#include "ui_login.h"
+#include "toolbox.h"
+#include "encrypt.h"
+Login::Login(QTcpSocket*hostfd,QWidget *parent) :
+    QDialog(parent),sock(hostfd),
+    ui(new Ui::Login)
+{
+    ui->setupUi(this);
+    setWindowFlags(Qt::FramelessWindowHint);
+}
+
+Login::~Login()
+{
+    delete ui;
+}
+
+void Login::on_loginBtn_clicked()
+{
+    if (ui->IDEdit->text().isEmpty()||ui->pass_Edit->text().isEmpty())
+        //如果lineEditUserID控件内容为空，提示用户错误
+       {
+           QMessageBox::information(this, tr("错误"), tr("用户ID不能为空"));
+           ui->IDEdit->setFocus();
+       }
+       else
+       {
+           bool ok;
+           userid =  ui->IDEdit->text().toInt(&ok);
+           if (!ok)//如果lineEditUserID控件内容不是数字，提示用户错误
+           {
+              QMessageBox::information(this, tr("错误"), tr("用户ID必须是数字"));
+              ui->IDEdit->setFocus();
+           }
+           else if(userid < 10000||userid > 99999)//如果用户输入的ID ;10000 - 99999提示错误
+           {
+               QMessageBox::information(this, tr("错误"), tr("ID ;10000 - 99999"));
+               ui->IDEdit->setFocus();
+           }
+           else
+           {
+               userid =  ui->IDEdit->text().toInt();
+               passwd =  ui->pass_Edit->text();
+
+              Value val;
+              val["Msg_Type"] = LOGIN;
+              val["password"] = passwd.toStdString();
+              val["id"] = userid;
+             // val[5] = 100;
+
+              char * endata = (char*)val.toStyledString().c_str();
+              //qDebug()<<endata;
+             // qDebug()<<strlen(endata);
+              my_encrypt rsa_en(endata);
+             // qDebug()<<rsa_en.en<<endl;
+              int len = sock->write(rsa_en.en,255);//发送账号密码
+             // qDebug()<<"发送子节数：：："<<len<<endl;
+           }
+       }
+}
+
+void Login::on_eixtBtn_clicked()
+{
+     exit(1);
+}
+
+void Login::on_registbtn_clicked()
+{
+    if (ui->IDEdit->text().isEmpty()||ui->pass_Edit->text().isEmpty())
+        //如果lineEditUserID控件内容为空，提示用户错误
+       {
+           QMessageBox::information(this, tr("错误"), tr("用户ID不能为空"));
+           ui->IDEdit->setFocus();
+       }
+       else
+       {
+           bool ok;
+           userid =  ui->IDEdit->text().toInt(&ok);
+           if (!ok)//如果lineEditUserID控件内容不是数字，提示用户错误
+           {
+              QMessageBox::information(this, tr("错误"), tr("用户ID必须是数字"));
+              ui->IDEdit->setFocus();
+           }
+           else if(userid < 10000||userid > 99999)//如果用户输入的ID ;10000 - 99999提示错误
+           {
+               QMessageBox::information(this, tr("错误"), tr("ID ;10000 - 99999"));
+               ui->IDEdit->setFocus();
+           }
+           else
+           {
+              userid =  ui->IDEdit->text().toInt();
+              passwd = ui->pass_Edit->text();
+              Value val;
+              val["Msg_Type"] = REGIST;
+              val["password"] = passwd.toStdString();
+              val["id"] = userid;
+              char * endata = (char*)val.toStyledString().c_str();
+              my_encrypt rsa_en(endata);
+              sock->write(rsa_en.en,255);//发送账号密码注册
+           }
+       }
+
+}
---
+++ login.h
@@ -0,0 +1,41 @@
+﻿#ifndef LOGIN_H
+#define LOGIN_H
+
+#include <QDialog>
+#include <QWidget>
+#include <QMessageBox>
+#include <QTcpSocket>
+#include <QHostAddress>
+#include <QAbstractSocket>
+#include <json/json.h>
+
+using namespace std;
+using namespace Json;
+namespace Ui {
+class Login;
+}
+
+class Login : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit Login( QTcpSocket*hostfd,QWidget *parent = 0);
+    ~Login();
+
+private slots:
+    void on_loginBtn_clicked();
+
+    void on_eixtBtn_clicked();
+
+    void on_registbtn_clicked();
+
+public:
+    Ui::Login *ui;
+    int userid;//本人id
+    QString passwd;//本人密码
+
+    QTcpSocket  *sock;//与主机通信的SOCKET
+};
+
+#endif // LOGIN_H
